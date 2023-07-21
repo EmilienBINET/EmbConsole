c4cbfc40cc2533c6394827cd9843a90bdf2d6a9a
EmilienBINET <78504841+EmilienBINET@users.noreply.github.com>
2023-07-21 15:45:41 +0200

    FIX MinGW build

--- src/impl/Tools.cpp
+++ src/impl/Tools.cpp
@@ -7,12 +7,12 @@ namespace emb {
     namespace tools {
         namespace thread {
             void set_thread_name(std::thread& a_rThread, std::string const& a_strThreadName) {
-#ifdef _WIN32
+#if defined unix || defined __MINGW32__
+                pthread_setname_np(a_rThread.native_handle(), a_strThreadName.c_str());
+#elif defined _WIN32
                 WCHAR wstr[64]{};
                 MultiByteToWideChar(0, 0, a_strThreadName.c_str(), -1, wstr, 62);
                 SetThreadDescription(static_cast<HANDLE>(a_rThread.native_handle()), wstr);
-#else
-                pthread_setname_np(arThread.native_handle(), aThreadName.c_str());
 #endif
             }
         }
