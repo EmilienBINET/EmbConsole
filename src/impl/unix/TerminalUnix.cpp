682f7a27cf98084f885081b2aef02a8a2effbf56
Emilien BINET <e.binet@visionix.com>
2023-09-11 12:12:26 +0200

    FIX Console not shown when in headless mode

--- src/impl/unix/TerminalUnix.cpp
+++ src/impl/unix/TerminalUnix.cpp
@@ -19,7 +19,7 @@ namespace emb {
         //TerminalUnix::TerminalUnix(TerminalUnix&&) noexcept = default;
         TerminalUnix::~TerminalUnix() noexcept {
             _this = nullptr;
-            if(m_bStarted) {
+            if(m_bStarted && m_bInputEnabled) {
                 string key{};
                 while (read(key)) {
                 }
@@ -29,21 +29,23 @@ namespace emb {
         //TerminalUnix& TerminalUnix::operator= (TerminalUnix&&) noexcept = default;
 
         void TerminalUnix::start() noexcept {
-            // Terminal unix only starts if stdin is valid, meaning the software is not launched headless
-            if(isatty(fileno(stdin))) {
-                m_bStarted = true;
+            if(m_bStarted) {
                 TerminalAnsi::start();
-                struct termios old;
-                memset(&old, 0, sizeof(struct termios));
-                if(tcgetattr(STDIN_FILENO, &old) < 0) {
-                    perror("TerminalUnix::start(1)");
-                }
-                old.c_lflag &= ~ICANON;
-                old.c_lflag &= ~ECHO;
-                old.c_cc[VMIN] = 1;
-                old.c_cc[VTIME] = 0;
-                if(tcsetattr(STDIN_FILENO, TCSANOW, &old) < 0) {
-                    perror("TerminalUnix::start(2)");
+                // Terminal unix input only starts if stdin is valid, meaning the software is not launched headless
+                if(isatty(fileno(stdin))) {
+                    m_bInputEnabled = true;
+                    struct termios old;
+                    memset(&old, 0, sizeof(struct termios));
+                    if(tcgetattr(STDIN_FILENO, &old) < 0) {
+                        perror("TerminalUnix::start(1)");
+                    }
+                    old.c_lflag &= ~ICANON;
+                    old.c_lflag &= ~ECHO;
+                    old.c_cc[VMIN] = 1;
+                    old.c_cc[VTIME] = 0;
+                    if(tcsetattr(STDIN_FILENO, TCSANOW, &old) < 0) {
+                        perror("TerminalUnix::start(2)");
+                    }
                 }
                 Terminal::start();
                 std::signal(SIGWINCH, [](int){
@@ -57,13 +59,15 @@ namespace emb {
 
         void TerminalUnix::processEvents() noexcept {
             if(m_bStarted) {
-                string keys{};
-                string key{};
-                while (read(key)) {
-                    keys += key;
-                }
-                if (!keys.empty() && !parseTerminalSizeResponse(keys)) {
-                    processPressedKeyCode(keys);
+                if(m_bInputEnabled) {
+                    string keys{};
+                    string key{};
+                    while (read(key)) {
+                        keys += key;
+                    }
+                    if (!keys.empty() && !parseTerminalSizeResponse(keys)) {
+                        processPressedKeyCode(keys);
+                    }
                 }
                 processPrintCommands();
                 processUserCommands();
@@ -73,17 +77,19 @@ namespace emb {
         void TerminalUnix::stop() noexcept {
             if(m_bStarted) {
                 TerminalAnsi::stop();
-                struct termios old;
-                memset(&old, 0, sizeof(struct termios));
-                if (tcgetattr(STDIN_FILENO, &old) < 0) {
-                    perror("TerminalUnix::stop(1)");
-                }
-                old.c_lflag |= ICANON;
-                old.c_lflag |= ECHO;
-                old.c_cc[VMIN] = 1;
-                old.c_cc[VTIME] = 0;
-                if (tcsetattr(STDIN_FILENO, TCSANOW, &old) < 0) {
-                    perror("TerminalUnix::stop(2)");
+                if(m_bInputEnabled) {
+                    struct termios old;
+                    memset(&old, 0, sizeof(struct termios));
+                    if (tcgetattr(STDIN_FILENO, &old) < 0) {
+                        perror("TerminalUnix::stop(1)");
+                    }
+                    old.c_lflag |= ICANON;
+                    old.c_lflag |= ECHO;
+                    old.c_cc[VMIN] = 1;
+                    old.c_cc[VTIME] = 0;
+                    if (tcsetattr(STDIN_FILENO, TCSANOW, &old) < 0) {
+                        perror("TerminalUnix::stop(2)");
+                    }
                 }
                 std::signal(SIGWINCH, SIG_DFL);
                 softReset();
