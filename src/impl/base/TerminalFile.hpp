3a360a5f2001627215fa4b015379bbe1f8e55f1f
Emilien BINET <e.binet@visionix.com>
2023-09-15 15:41:05 +0200

    FIX Terminal file do not close file correctly

--- src/impl/base/TerminalFile.hpp
+++ src/impl/base/TerminalFile.hpp
@@ -18,7 +18,7 @@ namespace emb {
 
             void start() noexcept override;
             void processEvents() noexcept override;
-            void stop() noexcept override;
+            //void stop() noexcept override;
 
             bool supportsInteractivity() const noexcept override { return false; }
             bool supportsColor() const noexcept override { return false; }
