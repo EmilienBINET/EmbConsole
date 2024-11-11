a1d875a6e73db4c8406bce4189976df40e85d6e6
EmilienBINET <78504841+EmilienBINET@users.noreply.github.com>
2024-11-11 12:48:02 +0100

    ADD emb::tools::regex::replace implemented from PCRE2

--- src/impl/Tools.cpp
+++ src/impl/Tools.cpp
@@ -43,7 +43,8 @@ namespace emb {
 #define STR2STPR8(__str) reinterpret_cast<unsigned char const*>(__str.c_str())
 
             bool match(std::string const& a_strStringToTest, std::string const& a_strRegexPattern) {
-                return false;
+                std::vector<std::string> vstrMatches{};
+                return search(vstrMatches, a_strStringToTest, "^" + a_strRegexPattern + "$");
             }
             bool search(std::vector<std::string>& a_rvstrMatches, std::string const& a_strStringToTest, std::string const& a_strRegexPattern) {
                 bool bRes{ true };
@@ -74,7 +75,7 @@ namespace emb {
                     (void)ulErrorOffset;
                 }
 
-                // Then trie to match the compiled regex against the string to test
+                // Then try to match the compiled regex against the string to test
                 if (bRes) {
                     // Using this function ensures that the block is exactly the right size for
                     // the number of capturing parentheses in the pattern
@@ -122,7 +123,76 @@ namespace emb {
                 return bRes;
             }
             std::string replace(std::string const& a_strStringToTest, std::string const& a_strRegexPattern, std::string const& a_strStringReplacement) {
-                return a_strStringToTest;
+                bool bRes{ true };
+                std::string strRes{};
+                pcre2_code* pRegex{ NULL };
+
+                // Start by compiling the regular expression
+                if (bRes) {
+                    int iErrorCode{ 0 };
+                    PCRE2_SIZE ulErrorOffset{ 0 };
+
+                    // Compile regex
+                    pRegex = pcre2_compile(
+                        STR2STPR8(a_strRegexPattern),   // The pattern to compile
+                        PCRE2_ZERO_TERMINATED,          // Indicates that the pattern is zero-terminated
+                        0,                              // Default options
+                        &iErrorCode,                    // For error number
+                        &ulErrorOffset,                 // For error offset
+                        NULL                            // Use default compile context
+                    );
+
+                    // Succeded if the result is not NULL
+                    bRes = NULL != pRegex;
+
+                    // We do not use error code here
+                    (void)iErrorCode;
+                    (void)ulErrorOffset;
+                }
+
+                // Then try to replace the compiled regex against the string to test
+                if (bRes) {
+                    PCRE2_UCHAR pOutput[1024]{ "" };
+                    PCRE2_SIZE ulOutputLen = sizeof(pOutput) / sizeof(PCRE2_UCHAR);
+
+                    // Substitute each occurence
+                    int iRes = pcre2_substitute(
+                        pRegex,                             // The compiled pattern
+                        STR2STPR8(a_strStringToTest),       // The subject string
+                        a_strStringToTest.length(),         // The length of the subject
+                        0,                                  // Offset in the subject at which to start matching
+                        PCRE2_SUBSTITUTE_GLOBAL | PCRE2_SUBSTITUTE_EXTENDED, // Option bits
+                        NULL,                               // No match data needed
+                        NULL,                               // Use default match context
+                        STR2STPR8(a_strStringReplacement),  // Points to the replacement string
+                        a_strStringReplacement.length(),    // Length of the replacement string
+                        pOutput,                            // Points to the output buffer
+                        &ulOutputLen                        // Points to the length of the output buffer
+                    );
+
+                    // Succedded if no negative code
+                    if (iRes > 0) {
+                        strRes = reinterpret_cast<char const*>(pOutput);
+                        bRes = true;
+                    }
+                    else if (iRes >= 0) {
+                        // no substitution but no error
+                        strRes = a_strStringToTest;
+                        bRes = true;
+                    }
+                    else {
+                        // error
+                        bRes = false;
+                    }
+                }
+
+                // And clean memory
+                if (pRegex) {
+                    pcre2_code_free(pRegex);
+                    pRegex = NULL;
+                }
+
+                return strRes;
             }
         }
 #endif
